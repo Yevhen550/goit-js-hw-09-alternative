be8f53f2ee2f43939c0879cbe12e747cabfda6eb
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-01 19:13:19 +0300

    first task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartBtn() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  startBtn.classList.remove('hover');
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled', '');
+}
