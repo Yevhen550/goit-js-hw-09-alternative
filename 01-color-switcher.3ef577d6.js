6ab42af25f410b86576822b030b7d20008c897f4
Yevhen550 <111044917+Yevhen550@users.noreply.github.com>
2024-05-01 16:13:42 +0000

    Deploying to gh-pages from @ Yevhen550/goit-js-hw-09-alternative@be8f53f2ee2f43939c0879cbe12e747cabfda6eb 🚀

---
+++ 01-color-switcher.3ef577d6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled",""),t.classList.remove("hover")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.3ef577d6.js.map
