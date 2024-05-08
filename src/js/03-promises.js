ab85a487ee83ff6ab4d5a2c7a52a2f3b61ca0789
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-08 12:47:42 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +8,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
