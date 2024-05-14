1a3866b8edd899a15c8c60f5ccd56624cbfa5915
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-14 13:26:11 +0300

    promis-task all dun

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,9 +2,11 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 const btnSubmit = document.querySelector('button');
-const delayInput = document.querySelector('[name="delay"]');
-const stepDelayInput = document.querySelector('[name="step"]');
-const amountInput = document.querySelector('[name="amount"]');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+form.style.display = 'inline-grid';
 
 //*****************==========================================================================
 //*
@@ -15,30 +17,52 @@ const amountInput = document.querySelector('[name="amount"]');
 //*
 //*****************===========================================================================
 
-let amount = amountInput.value;
+btnSubmit.addEventListener('click', onSubmitBtn);
 
-form.addEventListener('submit', createPromise);
+function onSubmitBtn(ev) {
+  ev.preventDefault();
+
+  if (delay.value === '' || step.value === '' || amount.value === '') {
+    return Notiflix.Notify.warning('Please fill in all fields');
+  } else if (+delay.value < 0 || +step.value < 0 || +amount.value <= 0) {
+    return Notiflix.Notify.warning(
+      `Numbers couldn't be negativ, amount field have to be more then 0`
+    );
+  }
+
+  createPromise(1, delay.value)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  let delayCount = +delay.value + +step.value;
+  for (let i = 1; i < +amount.value; i += 1) {
+    createPromise(i + 1, delayCount)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`)
+      );
+    delayCount += +step.value;
+  }
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setInterval(() => {
       if (shouldResolve) {
-        resolve('✅ Fulfilled');
+        resolve({ position: position, delay: delay });
       } else {
-        reject('❌ Rejected');
+        reject({ position: position, delay: delay });
       }
     }, delay);
   });
 }
-
-createPromise(2, 1500)
-  .then(res => console.log(res))
-  .catch(rej => console.log(rej));
-
-console.log(amount);
-
-// console.log('%c color:red', 'color:red; font-size:30px;');
 
 //*==========================================================================================
 //*
@@ -49,13 +73,3 @@ console.log(amount);
 //*  - виконати або відхилити.
 //*
 //*===========================================================================================
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-// Notiflix.Notify.info('Cogito ergo sum');
